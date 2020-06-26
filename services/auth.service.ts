c42618292b9475b153c75bb97f91db07566a6b72
ouyassa <63970320+ouyassa@users.noreply.github.com>
2020-06-26 21:05:16 +0200

    services/providers

---
+++ services/auth.service.ts
@@ -0,0 +1,41 @@
+import * as firebase from 'firebase';
+
+export class AuthService {
+  isAuth = false;
+
+  constructor() {
+    
+  }
+  createNewUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+}
+    
+connexionUser(email: string, password: string) {
+  return new Promise(
+    (resolve, reject) => {
+      firebase.auth().signInWithEmailAndPassword(email, password).then(
+        () => {
+          resolve();
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    }
+  );
+}
+      signOut() {
+        firebase.auth().signOut();
+    }
+}
